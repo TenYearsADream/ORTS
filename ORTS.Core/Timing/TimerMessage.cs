8564201da9c68f5fd4de59cc3ef161e526ed1e1f
Chris Lane <chris@lane.org.uk>
2011-06-07 21:30:12 +0100

    added timing classes

---
+++ ORTS.Core/Timing/TimerMessage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ORTS.Core.Timing
+{
+    public class TimerMessage
+    {
+        public String Message { get; set; }
+        public TickTime CurrentTickTime { get; set; }
+    }
+}
